146f3c6fdb3b344b51f290053f91e7c9675388a6
andrewrocke <90034200+andrewrocke@users.noreply.github.com>
2025-02-12 19:41:09 +0000

    Create split_data.py

---
+++ ML/YDF/split_data.py
@@ -0,0 +1,27 @@
+
+import numpy as np
+import pandas as pd
+
+
+
+
+ds = pd.read_csv("ML Test/output_bottom_singlelabel_add_del_NST.csv")
+print("total dataframe :",ds.shape)
+print(list(ds))
+print(ds.head())
+#X=ds['sAddress','rAddress']
+#y= ds['IT_B_Label']
+
+#X_train, X_test, y_train,  y_test = train_test_split(X,y ,  
+                          #random_state=104, 
+                          #train_size=0.8, shuffle=True)
+
+mask = np.random.rand(len(ds)) < 0.8
+train_ds = ds[mask] 
+test_ds = ds[~mask] 
+
+train_ds.shape, test_ds.shape 
+
+train_ds.to_csv("train_ds.csv")
+
+test_ds.to_csv("test_ds.csv")
